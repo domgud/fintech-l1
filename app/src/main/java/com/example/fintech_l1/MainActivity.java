217a9f4a0516b888c5a1a7a9d9ca4d0da0c02cd9
domgud <dgdomis52@gmail.com>
2024-03-11 13:46:32 +0200

    Revert "Revert me"
    This reverts commit 2b4778ce2b1fb885510e57b95a75a6fd5fe8e281.

--- app/src/main/java/com/example/fintech_l1/MainActivity.java
+++ app/src/main/java/com/example/fintech_l1/MainActivity.java
@@ -17,7 +17,6 @@ public class MainActivity extends AppCompatActivity {
 
     // used for random color generation
     private final Random random = new Random();
-    //hello, this will be reverted.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
